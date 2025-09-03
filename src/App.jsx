bb4a117cc7d1c42a84973097ef95d8400fbdfeb4
bijnesh kumar <bijneshkumar10@gmail.com>
2025-09-03 09:39:15 +0530

    add basename in browserRouter in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Redux-toolkit-sm-project/">
 
       <Navbar />
       <Routes>
